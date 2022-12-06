06d8d844efef9b8f10d3d1e83a16e78536d0f9d3
Jordan Thevenow-Harrison <jtth@jtth.net>
2022-12-05 23:51:56 -0700

    refine day5 transposition test

--- src/test/java/net/jtth/Day5Test.java
+++ src/test/java/net/jtth/Day5Test.java
@@ -19,20 +19,20 @@ public class Day5Test {
   }
 
   @Test
-  @DisplayName("transposing lists")
+  @DisplayName("transposing lists for rectangular 2d lists")
   void transposeList() {
     List<List<Integer>> testList = List.of(List.of(1, 2), List.of(1, 2, 3), List.of(1, 2, 3));
     List<List<Integer>> transposedList = List.of(List.of(1, 1, 1), List.of(2, 2, 2), List.of(3, 3));
     assertEquals(transposedList, Day5.transposeListOfLists(testList), "list should transpose");
 
     List<List<String>> transposedExampleList = List.of(
-        List.of("N", "Z"),
+        List.of("", "N", "Z"),
         List.of("D", "C", "M"),
-        List.of("P")
+        List.of("", "", "P")
     );
     List<List<String>> exampleList = List.of(
-        List.of("D"),
-        List.of("N", "C"),
+        List.of("", "D", ""),
+        List.of("N", "C", ""),
         List.of("Z", "M", "P")
     );
     assertEquals(transposedExampleList, Day5.transposeListOfLists(exampleList),
